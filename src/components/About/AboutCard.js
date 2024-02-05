153955e30239ccd8823ff51d131b0bcc3c5480d0
Sanket Ghorpade <69021392+SanketGhorpade1999@users.noreply.github.com>
2024-02-06 02:02:52 +0530

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -16,9 +16,10 @@ function AboutCard() {
             I have completed Bachelor's of Engineering (B.E.) in Information Technology at University of
             Pune.
             <br />
-              About my internship I have completed Front-end development Intern in Siddhant College of Engineering
-              where my task was to devloped and modify existing college website in HTML, CSS and JavaScript collaborating with 
-                Elite Software Solutions Pvt. Ltd.
+              About my internship I have completed Golang Developer Intern in Suven Consultants & Technologies Pvt. Ltd.
+              where my task was to devloped and modify and Resolved 250+ bugs, improved code efficiency by 95%, and enhanced code quality by incorporating Object-Oriented Patterns, 
+              resulting in a 92% increase in overall code quality. During the internship, delved deep into the Go language, exploring OOP Concepts, Functions and Methods, 
+              understanding of Gin Framework, File System in Golang, Dynamic Reflection in Go, etc.
             <br />
                 <br/>
                 <br/>
